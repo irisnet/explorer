1d26571caca37f3759bf59bb9c92fa26953bab75
zhangzhiqiang <745124335@aa.com>
2018-04-16 09:31:47 +0800

    1:bug fix :优化交易查询rest接口时，参数校验错误，导致部分交易查询不到 IRISE-18 2:发布脚本优化(原因:服务器没有编译环境，只能先本地打包，上传，在发布。现在将发布流程命令分解)

--- setup/start.sh
+++ setup/start.sh
@@ -1,16 +1,52 @@
 #!/usr/bin/env bash
-####build irisplorer
-cd ../server/
-make irisplorer
+buildServer(){
+    cd ../server/
+    if [ ! -f "./build/irisplorer" ];then
+    echo "make irisplorer"
+    make irisplorer
+    else
+    echo "irisplorer has existed,skip [make irisplorer]"
+    fi
 
-### make irisplorer image
-docker build -t irisplorer:0.1.0 -f ./Dockerfile .
+    echo "build docker images[irisplorer:0.1.0]"
+    docker build -t irisplorer:0.1.0 -f ./Dockerfile .
+}
 
-### make irisplorer ui image
-cd ../ui/
-yarn install
-docker build -t irisplorer-ui:0.1.0 -f ./Dockerfile .
+buildUi(){
+    cd ../ui/
+    yarn install
+    echo "build docker images[irisplorer-ui:0.1.0]"
+    docker build -t irisplorer-ui:0.1.0 -f ./Dockerfile .
+}
 
-cd ../setup/
-docker network create irisplorernet
-docker-compose -f ./docker-compose.yml up -d
+up(){
+    cd ../setup/
+    docker network create irisplorernet
+    echo "start docker container..."
+    docker-compose -f ./docker-compose.yml up -d
+}
+
+down(){
+    cd ../setup/
+    echo "stop docker container ..."
+    docker-compose -f ./docker-compose.yml down
+}
+
+execute(){
+    if [ $1 = "server" ];then
+        buildServer
+    elif [ $1 = "ui" ]; then
+        buildUi
+    elif [ $1 = "u" ]; then
+        up
+    elif [ $1 = "d" ]; then
+        down
+    elif [ $1 = "all" ]; then
+        buildServer
+        buildUi
+        up
+    fi
+}
+
+echo "exec $1"
+execute $1
