cde5c3651edb22aae027643dfef23868b67a3f59
chengwenxi <a345407031#>
2018-07-09 16:05:53 +0800

    add editorconfig and blockPage,faucetPage

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="home">
-    <Header />
+    <Header/>
+    <div class="content">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Header from '@/components/AppHeader.vue'; // @ is an alias to /src
+  import {Component, Vue} from 'vue-property-decorator';
+  import Header from '@/components/AppHeader.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    Header,
-  },
-})
-export default class Home extends Vue {}
+  @Component({
+    components: {
+      Header,
+    },
+  })
+  export default class Home extends Vue {
+  }
 </script>
+
+<style>
+  .content {
+    background: darkgrey;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+</style>
