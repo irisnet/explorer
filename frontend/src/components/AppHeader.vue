cba8cec95285bce55f4b1ea8f983bbc0cddc9887
lvshenchao <rumble@bianjie.ai>
2018-07-12 15:02:26 +0800

    #IRISXP-37 头部导航栏修改

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <!--头部页面-->
 <template>
-  <div class="app_header">
+  <div :class="appHeaderVar">
     <header class="app_header_person_computer" v-show="devicesShow === 1">
       <div class="header_top">
         <div class="imageWrap">
@@ -49,14 +49,14 @@
       <div class="use_feature_mobile" v-show="featureShow">
         <span class="feature_btn_mobile feature_nav" @click="featureButtonClick('/home')">Home</span>
         <span class="feature_btn_mobile feature_nav" @click="featureButtonClick('/blocks')">Blocks</span>
-        <span class="feature_btn_mobile feature_nav" @click="transactionShow =! transactionShow">Transactions</span>
+        <span class="feature_btn_mobile feature_nav feature_arrow" @click="transactionShow =! transactionShow">Transactions</span>
         <span class="feature_btn_mobile feature_subNav" v-show="transactionShow"
               @click="featureButtonClick('/recent_transactions')">Recent Transactions</span>
         <span class="feature_btn_mobile feature_subNav" v-show="transactionShow"
               @click="featureButtonClick('/transfer_transactions')">Transfer Transactions</span>
         <span class="feature_btn_mobile feature_subNav" v-show="transactionShow"
               @click="featureButtonClick('/delegate_transactions')">Delegate Transactions</span>
-        <span class="feature_btn_mobile feature_nav" @click="validatorsShow =! validatorsShow">Validators</span>
+        <span class="feature_btn_mobile feature_nav feature_arrow" @click="validatorsShow =! validatorsShow">Validators</span>
         <span class="feature_btn_mobile feature_subNav" v-show="validatorsShow"
               @click="featureButtonClick('/validators')">Validators</span>
         <span class="feature_btn_mobile feature_subNav" v-show="validatorsShow"
@@ -65,8 +65,10 @@
       </div>
       <div class="search_input_mobile">
         <input type="text" class="search_input"
-               placeholder="Search by Address / Txhash / Block"
-        >
+               v-model="searchInputValue"
+               @keyup.enter="onInputChange"
+               placeholder="Search by Address / Txhash / Block">
+        <i class="search_icon" @click="getData(searchInputValue)"></i>
       </div>
     </div>
   </div>
@@ -80,6 +82,7 @@
         devicesWidth: window.innerWidth,
         devicesShow: 1,//1是显示pc端，0是移动端
         searchValue: '',
+        appHeaderVar:'person_computer_header_var',
         featureShow: false,//是否显示功能菜单栏
         transactionShow: false,//点击显示Transactions菜单
         validatorsShow: false,//点击显示validators菜单
@@ -90,8 +93,10 @@
     beforeMount() {
       if (this.devicesWidth > 500) {
         this.devicesShow = 1;
+        this.appHeaderVar = 'person_computer_header_var';
       } else {
         this.devicesShow = 0;
+        this.appHeaderVar = 'mobile_header_var';
       }
     },
     mounted() {
@@ -137,18 +142,21 @@
 <style lang="scss">
   @import '../style/mixin.scss';
 
-  .app_header {
-    height: 116px;
+  .person_computer_header_var{
+    height: 150px;
+  }
+  .mobile_header_var{
+    height:100px;
+  }
+  .person_computer_header_var, .mobile_header_var {
     width: 100%;
-    display: flex;
+    @include flex();
     flex-direction: column;
     align-items: center;
-    background: yellow;
     .app_header_person_computer {
       width: 80%;
       min-width: 600px;
       max-width: 1000px;
-      border: 1px solid red;
       justify-content: space-between;
       padding-top: 10px;
       .header_top{
@@ -156,7 +164,7 @@
         justify-content: space-between;
         .imageWrap {
           width: 100px;
-          height: 50px;
+          height: 78px;
           img {
             width: 100px;
             height: 50px;
@@ -179,14 +187,16 @@
             text-indent: 10px;
             outline: none;
             border: 1px solid #dddddd;
+            margin-top:20px;
           }
           .search_icon {
             position: absolute;
-            top: 5px;
+            top: 27px;
             right: 10px;
             width: 15px;
             height: 15px;
-            background: lawngreen;
+            background: url('../assets/search.svg') no-repeat;
+            cursor:pointer;
           }
         }
       }
@@ -201,18 +211,20 @@
       @include flex();
       flex-direction: column;
       align-items: center;
+      background: #EBEBEB;
       .navButton {
         width:80%;
         min-width: 600px;
         max-width: 1000px;
         .nav_item {
           display: inline-block;
-          height: 51px;
-          line-height: 50px;
+          height: 57px;
+          line-height: 54px;
           padding: 0 40px;
           text-align: center;
           font-size: 14px;
           cursor: pointer;
+          color:#cccccc;
         }
         .nav_item_active{
           border-bottom:2px solid #3190e8;
@@ -220,14 +232,14 @@
         }
         .m-2 {
           margin: 0 !important;
-          height:50px;
+          height:59px;
           button {
             padding: 0 40px;
             color:#555 !important;
           }
           .btn {
             background-color: transparent;
-            color: #dddddd;
+            color: #cccccc !important;
             border: none;
             font-weight: normal;
           }
@@ -242,13 +254,14 @@
       flex-direction: column;
       position: relative;
       height: 100px;
+      border-bottom:1px solid #cccccc;
       .feature_btn {
         position: absolute;
         width: 40px;
         height: 40px;
         top: 0;
         right: 0;
-        background: lawngreen;
+        background: url('../assets/menu.svg') no-repeat;
       }
       .image_wrap_mobile {
         width: 70vw;
@@ -264,6 +277,7 @@
         @include flex();
         flex-direction: column;
         align-items: center;
+        position:relative;
         input::-webkit-input-placeholder {
           text-align: center;
           font-size: 14px;
@@ -280,21 +294,34 @@
             outline: none;
           }
         }
+        .search_icon{
+          position: absolute;
+          top: 6px;
+          right: 18px;
+          width: 15px;
+          height: 15px;
+          background: url('../assets/search.svg') no-repeat;
+          cursor:pointer;
+        }
       }
       .use_feature_mobile {
         position: absolute;
         width: 100%;
         top: 100px;
         left: 0;
-        background: #3190e8;
+        background: #f2f2f2;
         @include flex();
         flex-direction: column;
         .feature_btn_mobile {
-          border-bottom: 1px solid #eee;
+          border-bottom: 1px solid #cccccc;
           height: 39px;
           line-height: 39px;
           padding-left: 15px;
           color: #555;
+        }
+        .feature_arrow{
+          position:relative;
+          background: url('../assets/arrow-bottom.svg') no-repeat 97% 12px;
         }
         .feature_subNav {
           padding-left: 30px;
