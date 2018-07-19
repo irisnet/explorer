7477e0453dcca80fa035112dc2a34156ce5908e3
lvshenchao <rumble@bianjie.ai>
2018-07-19 16:00:21 +0800

    #IRISXP-41 Home Page Validator List Module

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
           '/api':{
-            target:'http://192.168.150.110:8080',
+            target:'http://192.168.150.116:8080',
             secure:false,
           }
         }
