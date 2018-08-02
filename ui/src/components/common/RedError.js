2f78ac9380b08e67bf1142e44b5fc38259fb07bc
zhangyelong <yelong@bianjie.ai>
2018-08-02 14:06:32 +0800

    delete deprecated ui

--- ui/src/components/common/RedError.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Message } from "semantic-ui-react"
-
-export default class RedError extends React.Component {
-  render() {
-    return <Message negative>{this.props.message}</Message>
-  }
-}
