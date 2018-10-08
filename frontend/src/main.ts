2168e4e77403e98e7cef971a3a4f59d9c4e07a52
zhjibi742626083 <jinbiao@bianjie.ai>
2018-10-08 14:15:49 +0800

    #RBAPP-674 explorer去掉错误的依赖

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './registerServiceWorker'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
