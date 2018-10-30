f5e82fb9b23670e8ddc16407bda6a820999b4604
zhjibi742626083 <jinbiao@bianjie.ai>
2018-10-30 19:59:42 +0800

    #IRISXP-322
    service worker cache问题修复

--- frontend/src/registerServiceWorker.ts
+++ frontend/src/registerServiceWorker.ts
@@ -2,7 +2,7 @@
 
 import { register } from 'register-service-worker'
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
       console.log(
