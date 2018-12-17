d23811629a23d17f6f9cf2e396c6cf70704c25eb
xiaoxianming <xianming@bianjie.ai>
2018-12-17 15:21:31 +0800

    update addr prefix

--- frontend/package.js
+++ frontend/package.js
@@ -8,7 +8,7 @@ function list (val) {
 }
 
 console.log('Replacing environments ...');
-let accAddr = "xaa", accPub = "xap", valAddr = "xva";
+let accAddr = "faa", accPub = "fap", valAddr = "fva";
 
 
 
