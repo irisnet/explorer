e1b3561006f1775ec375988ad84a5e3136792b9c
zhangzhiqiang <745124335@aa.com>
2019-01-25 15:07:40 +0800

    code refactor

--- backend/model/account.go
+++ backend/model/account.go
@@ -7,6 +7,7 @@ import (
 type AccountVo struct {
 	Amount          document.Coins `json:"amount"`
 	WithdrawAddress string         `json:"withdrawAddress"`
+	Address         string         `json:"address"`
 	Deposits        document.Coin  `json:"deposits"`
 	IsProfiler      bool           `json:"isProfiler"`
 }
