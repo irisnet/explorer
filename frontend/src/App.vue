1dee2902da9d4307ce5a4533c463c79b7942d9ae
zhjibi742626083 <jinbiao@bianjie.ai>
2019-01-29 10:47:06 +0800

    #IRISXP-480

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -32,7 +32,7 @@
           </div>
           <div class="footer_center">
             <a href="https://www.irisnet.org/" target="_blank">
-              <img src="./assets/IRIS-logo.png" alt="">
+              <img src="./assets/irisnet.png" alt="">
             </a>
           </div>
           <div class="footer_right" :class="footerRightVar">
@@ -345,13 +345,10 @@
               margin-top: 0.1rem;
               font-size: 0.14rem;
             }
-
           }
-
         }
       }
     }
-
   }
   pre{
     font-family: Arial !important;
