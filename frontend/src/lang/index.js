9d0cd2c0e6364ae4cfda53b27becd4abefaa90d6
zhjibi742626083 <jinbiao@bianjie.ai>
2019-03-21 15:49:56 +0800

    #IRISXP-545

---
+++ frontend/src/lang/index.js
@@ -0,0 +1,15 @@
+export default {
+  home:{
+      blockHeight: 'Block Height',
+      transactions: 'Transactions',
+      ageTime: 'Average Block Time',
+      votingPower: 'Voting Power',
+      bondedTokens: 'Bonded Tokens',
+      mainnet: 'Mainnet',
+      testnet: 'FUXI Testnet',
+      catsNet: 'NYAN_CATS Testnet'
+    
+    
+  }
+}
+
