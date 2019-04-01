81e457613f759f5c822fef7912f27ecd4c8c048e
zhjibi742626083 <jinbiao@bianjie.ai>
2019-04-01 10:21:20 +0800

    Merge remote-tracking branch 'hotfix/0.9.0' into develop

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,11 +2,6 @@ module.exports = {
     devServer: {
         proxy: {
           '/api':{
-            // target:'http://dev.irisplorer.io',
-            target:'http://192.168.150.31:30201/',
-            // target:'http://35.236.146.181:8080/',
-            // target:'http://34.80.228.1:30201/',
-            // target:'http://35.220.138.85:8080/',
             // target:'http://localhost:8080',
             secure:false,
           }
