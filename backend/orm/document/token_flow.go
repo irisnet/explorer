7841beb0eeb9a47b5e2afa3540e9c121ac1907d7
liwei <liwei@bianjie.ai>
2019-04-22 10:16:42 +0800

    fix json

--- backend/orm/document/token_flow.go
+++ backend/orm/document/token_flow.go
@@ -7,16 +7,16 @@ const (
 )
 
 type TokenFlow struct {
-	BlockHeight int64      `bson:"block_height"`
-	BlockHash   string     `bson:"block_hash"`
-	TxHash      string     `bson:"tx_hash"`
-	From        string     `bson:"from"`
-	To          string     `bson:"to"`
-	Amount      store.Coin `bson:"amount"`
-	Fee         store.Fee  `bson:"fee"`
-	TxInitiator string     `bson:"tx_initiator"`
-	TxType      string     `bson:"tx_type"`
-	FlowType    string     `bson:"flow_type"`
-	Status      string     `bson:"status"`
-	Timestamp   string     `bson:"timestamp"`
+	BlockHeight int64      `bson:"block_height" json:"block_height"`
+	BlockHash   string     `bson:"block_hash" json:"block_hash"`
+	TxHash      string     `bson:"tx_hash" json:"tx_hash"`
+	From        string     `bson:"from" json:"from"`
+	To          string     `bson:"to" json:"to"`
+	Amount      store.Coin `bson:"amount" json:"amount"`
+	Fee         store.Fee  `bson:"fee" json:"fee"`
+	TxInitiator string     `bson:"tx_initiator" json:"tx_initiator"`
+	TxType      string     `bson:"tx_type" json:"tx_type"`
+	FlowType    string     `bson:"flow_type" json:"flow_type"`
+	Status      string     `bson:"status" json:"status"`
+	Timestamp   string     `bson:"timestamp" json:"timestamp"`
 }
