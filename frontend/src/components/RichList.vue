fa5374a9e6855cb26324d6fe1a195cd686004a88
zhjibi742626083 <jinbiao@bianjie.ai>
2019-05-07 16:42:47 +0800

    #IRISXP-602

--- frontend/src/components/RichList.vue
+++ frontend/src/components/RichList.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="top_list_container">
         <div class="top_list_content">
-          <div class="top_list_title_wrap" style="">
+          <div class="top_list_title_wrap">
             <h5 class="top_list_time_content"><span v-show="latestTime">Updated ：{{latestTime}}+UTC</span></h5>
             <h5 class="top_list_assets_content"><span><i>*</i> The assets include the delegated, unbonding and balance.</span></h5>
           </div>
