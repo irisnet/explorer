9a94e51738e85c8de4125bae494df49db195d928
wade-liwei <liwei@bianjie.ai>
2019-05-16 14:30:33 +0800

    replace fee with actual_fee in the  coinflow

--- backend/orm/document/token_flow.go
+++ backend/orm/document/token_flow.go
@@ -13,7 +13,7 @@ type TokenFlow struct {
 	From        string    `bson:"from" json:"from"`
 	To          string    `bson:"to" json:"to"`
 	Amount      Coin      `bson:"amount" json:"amount"`
-	Fee         Fee       `bson:"fee" json:"fee"`
+	ActualFee   ActualFee `bson:"actual_fee" json:"actual_fee"`
 	TxInitiator string    `bson:"tx_initiator" json:"tx_initiator"`
 	TxType      string    `bson:"tx_type" json:"tx_type"`
 	FlowType    string    `bson:"flow_type" json:"flow_type"`
