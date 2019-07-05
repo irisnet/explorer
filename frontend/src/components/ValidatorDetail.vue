1cbf39aa6f81c7d018ab2cd66748c0e8858a156f
wenwu <wenwu@bianjie.ai>
2019-07-05 18:19:38 +0800

    modify validator Delegator Bonded

--- frontend/src/components/ValidatorDetail.vue
+++ frontend/src/components/ValidatorDetail.vue
@@ -611,13 +611,14 @@ export default {
                                 : "";
                             data.jailed_until === "" &&
                                 delete this.validatorInfo["Jailed Until"];
+                            let delegator_tokens = `${Number(data.bonded_tokens) - (Number(data.bonded_tokens) / Number(data.delegator_shares)) * Number(data.self_bonded)}`;
                             this.validatorInfo[
                                 "Delegator Bonded"
                             ] = `${this.$options.filters.amountFromat(
-                                data.bonded_stake,
+                                delegator_tokens,
                                 Constants.Denom.IRIS.toUpperCase()
                             )} (${this.formatPerNumber(
-                                (Number(data.bonded_stake) /
+                                (Number(delegator_tokens) /
                                     Number(data.bonded_tokens)) *
                                     100
                             )} %)`;
