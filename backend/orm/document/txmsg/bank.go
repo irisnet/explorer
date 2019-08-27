0032f3a65c47d553797c6bdc187c88acfd951039
kaifei <kaifei@bianjie.ai>
2019-08-27 14:54:28 +0800

    remove unused files

--- backend/orm/document/txmsg/bank.go
+++
@@ -1,6 +0,0 @@
-package txmsg
-
-type TxMsgSetMemoRegexp struct {
-	Owner      string `bson:"owner"`
-	MemoRegexp string `bson:"memo_regexp"`
-}
--- backend/orm/document/txmsg/rand.go
+++
@@ -1,6 +0,0 @@
-package txmsg
-
-type TxMsgRequestRand struct {
-	Consumer      string `bson:"consumer"`       // request address
-	BlockInterval int64  `bson:"block_interval"` // block interval after which the requested random number will be generated
-}
