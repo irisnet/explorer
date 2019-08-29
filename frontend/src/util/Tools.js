0839b1b96b34107e744c7afe614dae3bd32560cb
zhjibi742626083 <jinbiao@bianjie.ai>
2019-08-29 12:10:27 +0800

    substr amount number

--- frontend/src/util/Tools.js
+++ frontend/src/util/Tools.js
@@ -177,10 +177,10 @@ export default class Tools{
 			if(splitString.length > splitNum){
 				return str.split(".")[0] + '.' +  splitString.substr(0,splitNum)
 			}else {
-				return str.split(".")[0] + '.' + splitString.padEnd(4, "0")
+				return str.split(".")[0] + '.' + splitString.padEnd(2, "0")
 			}
 		}else {
-			return str + '.0000'
+			return str + '.00'
 		}
 	}
 	
@@ -397,7 +397,7 @@ export default class Tools{
 				if(item.amount){
 					if(item.amount instanceof Array && item.amount.length > 0){
 						if(item.amount[0].denom && item.amount[0].amount && item.amount[0].denom === Constant.Denom.IRISATTO){
-							transferAmount = item.amount[0].formatAmount = item.amount[0].amount > 0 ? Tools.numberMoveDecimal(item.amount[0].amount) : item.amount[0].amount;
+							transferAmount = item.amount[0].formatAmount = item.amount[0].amount > 0 ? Tools.formatStringToFixedNumber(String(Tools.numberMoveDecimal(item.amount[0].amount)),2) : item.amount[0].amount;
 							tokenId = item.amount[0].tokenId = Constant.Denom.IRIS.toLocaleUpperCase();
 							Amount = item.amount.map(listItem=>`${listItem.formatAmount} ${Tools.formatDenom(listItem.denom).toUpperCase()}`).join(',');
 						}else if(item.amount[0].denom && item.amount[0].amount && item.amount[0].denom !== Constant.Denom.IRISATTO){
@@ -407,13 +407,13 @@ export default class Tools{
 							transferAmount = item.amount[0].formatAmount = item.amount[0].amount;
 							tokenId = item.amount[0].tokenId = item.amount[0].denom.toLocaleUpperCase();
 							if(item.type === 'BeginUnbonding' || item.type === 'BeginRedelegate'){
-								item.amount[0].formatAmount = item.amount[0].amount > 0 ? Tools.numberMoveDecimal(item.amount[0].amount) : item.amount[0].amount;
+								item.amount[0].formatAmount = item.amount[0].amount > 0 ? Tools.formatStringToFixedNumber(String(Tools.numberMoveDecimal(item.amount[0].amount)),2): item.amount[0].amount;
 								Amount = item.amount.map(listItem => `${listItem.formatAmount} SHARES`).join(',');
 							}
 						}
 					}else if(item.amount && Object.keys(item.amount).includes('amount') && Object.keys(item.amount).includes('denom')){
 						if(item.amount.denom === Constant.Denom.IRISATTO){
-							transferAmount = Tools.numberMoveDecimal(item.amount);
+							transferAmount = Tools.formatStringToFixedNumber(String(Tools.numberMoveDecimal(item.amount)),2);
 							tokenId = Constant.Denom.IRIS.toLocaleUpperCase();
 							Amount = `${transferAmount}  ${Tools.formatDenom(item.amount.denom).toUpperCase()}`;
 							
