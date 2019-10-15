3dc3a9a93ba0bb988ed456dbc76c652b477134c1
黄 伟昌 <weichang@bianjie.ai>
2019-10-15 16:44:03 +0800

    add the asset token unit covert

--- backend/orm/document/asset_test.go
+++ backend/orm/document/asset_test.go
@@ -3,6 +3,7 @@ package document_test
 import (
 	"github.com/irisnet/explorer/backend/orm/document"
 	"testing"
+	"encoding/json"
 )
 
 func TestAsset_GetAllAssets(t *testing.T) {
@@ -14,4 +15,14 @@ func TestAsset_GetAllAssets(t *testing.T) {
 	for k, v := range allAsset {
 		t.Logf("k: %v \nv: %v\n", k, v)
 	}
+}
+
+func TestAssetToken_GetAssetTokenDetail(t *testing.T) {
+	allAsset, err := document.AssetToken{}.GetAssetTokenDetail("mondex.sun")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bytesmsg,_ := json.Marshal(allAsset)
+	t.Log(string(bytesmsg))
 }
