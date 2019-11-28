c5010dceed1f417ec49da7f9614bab6c047433f6
zhjibi742626083 <jinbiao@bianjie.ai>
2019-11-28 10:22:51 +0800

    fix transfer not display list

--- frontend/src/components/txs/MTxListPageTable.vue
+++ frontend/src/components/txs/MTxListPageTable.vue
@@ -74,12 +74,12 @@
             </template>
             <template slot-scope="{ row }" slot="Amount">
                 <div class="name_address">
-                    <div v-if="!row.Amount.includes('Tokens')">
+                    <div v-if="!row.Amount.toString().includes('Tokens')">
                             <span>
                                 <span>{{substrAmount(row.Amount)}}</span>
                             </span>
                     </div>
-                    <div  v-if="row.Amount.includes('Tokens')"  class="skip_route">
+                    <div  v-if="row.Amount.toString().includes('Tokens')"  class="skip_route">
                         <router-link :to="`/tx?txHash=${row.Tx_Hash}`">{{substrAmount(row.Amount)}}</router-link>
                     </div>
                     <span class="address" v-if="row.Amount.toString().length > 12">{{row.Amount}}</span>
