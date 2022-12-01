586d529d1294dcdc23d1f729a407098fe509e797
duanjie-cmd <18870420982@139.com>
2022-12-01 10:39:46 +0800

    update vue.config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,12 +1,28 @@
 //@ts-check
+const TerserPlugin = require('terser-webpack-plugin');
 console.log('env:',process.env);
 module.exports = {
-    devServer: {
-        proxy: {
-          '/api':{
-            target:'http://localhost:8080',
-            secure:false,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        secure: false,
+      },
+    }
+  },
+  configureWebpack: {
+    optimization: {
+      minimize: process.env.NODE_ENV === 'production',
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+              drop_debugger: true,
+            },
           },
-        }
+        }),
+      ],
     },
-};
+  },
+}
